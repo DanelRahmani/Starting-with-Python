128f433932b3a5719e5326b20bbea6089926ab94
Danel Rahmani <danel.rahmani@outlook.com>
2023-01-12 17:34:17 +0100

    On branch main Your branch is up to date with 'Starting-with-Python/main'. Changes to be committed: 	modified:   cat.py 	new file:   hogwarts.py

--- cat.py
+++ cat.py
@@ -1,2 +1,12 @@
-for i in range(3):
-    print("Meow")
+def main():
+    while True:                     # ask user for n
+        n = int(input("What's n?"))
+        if n > 0:
+            break
+    meow(n)
+
+def meow(n):
+        for i in range(n):          #makes cat output thoughts n times
+            print("Meow")         #outputs the cat's thoughts
+
+main()
