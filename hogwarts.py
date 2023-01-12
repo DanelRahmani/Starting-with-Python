782e6c1c26944bd7838123e7b7feadc8de9ece46
Danel Rahmani <danel.rahmani@outlook.com>
2023-01-12 18:00:40 +0100

    On branch main Your branch is up to date with 'Starting-with-Python/main'. 	modified:   hogwarts.py 	new file:   mario.py

--- hogwarts.py
+++ hogwarts.py
@@ -1,9 +1,8 @@
-students = {
-    "Hermione": "Gryffindor",
-    "Harry": "Gryffindor", 
-    "Ron": "Gryffindor",
-    "Draco": "Slytherin"
-}
-
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None}
+]
 for student in students:
-    print(student,"who lives at", students[student])
+    print(student["name"], student["house"], student["patronus"], sep=", ")
---
+++ mario.py
@@ -0,0 +1,8 @@
+def main():
+    print_square(3)
+
+def print_square(size): # prints square
+    for i in range(size):# for each row in square
+        print("#" * size)
+
+main() 
