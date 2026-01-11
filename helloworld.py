c485cead5e4bedef4ed45bc849ff11d32491dbac
Danel <danel.rahmani@outlook.com>
2026-01-11 20:37:49 +0100

    Small changes made to the repository

---
+++ helloworld.py
@@ -0,0 +1,28 @@
+# Day 1 - 30DaysOfPython Challenge
+
+print(3 + 4)             # addition(+)
+print(3 - 4)             # subtraction(-)
+print(3 * 4)             # multiplication(*)
+print(3 / 4)             # division(/)
+print(3 ** 4)            # exponential(**)
+print(3 % 4)             # modulus(%)
+print(3 // 4)            # Floor division operator(//)
+
+# Print Strings
+print('Danel')
+print('Rahmani')
+print('The Netherlands')
+print('I am enjoying 30 days of python')
+
+# Checking data types
+print(type(10))          # Int
+print(type(9.8))        # Float
+print(type(3.14))       # Float
+print(type(4 - 4j))      # Complex number
+print(type('Danel'))  # String
+print(type('Rahmani'))  # String
+print(type('The Netherlands'))       # Boolean
+print(type(['Danel', 'Rahmani','The Netherlands']))   # List
+print(type({'name':'Danel'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
